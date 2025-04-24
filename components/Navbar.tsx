3ac93cec4fcc0f0b4014b13f32216bc815d08b4a
Rakesh Baruah <rbaruah@crisisprevention.com>
2025-04-24 16:59:26 -0500

    feat: integrate theme provider and add theme toggle functionality in Navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,5 +1,7 @@
+"use client";
+
 import { Avatar, AvatarFallback, AvatarImage } from "@radix-ui/react-avatar";
-import { LogOut, Moon, Settings, SquareMenu, User } from "lucide-react";
+import { LogOut, Moon, Settings, Sun, User } from "lucide-react";
 import Link from "next/link";
 import {
   DropdownMenu,
@@ -9,18 +11,37 @@ import {
   DropdownMenuSeparator,
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
-
 import React from "react";
 import { Button } from "./ui/button";
+import { useTheme } from "next-themes";
 
 export default function Navbar() {
+  const { setTheme } = useTheme();
   return (
     <nav className="p-4 flex items-center justify-between">
       collapseButton
       <div className="flex items-center gap-4">
         <Link href="/">Dashboard</Link>
-        <Moon />
-
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <Button variant="outline" size="icon">
+              <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
+              <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+              <span className="sr-only">Toggle theme</span>
+            </Button>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent align="end">
+            <DropdownMenuItem onClick={() => setTheme("light")}>
+              Light
+            </DropdownMenuItem>
+            <DropdownMenuItem onClick={() => setTheme("dark")}>
+              Dark
+            </DropdownMenuItem>
+            <DropdownMenuItem onClick={() => setTheme("system")}>
+              System
+            </DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
         <DropdownMenu>
           <DropdownMenuTrigger>
             <Avatar>
