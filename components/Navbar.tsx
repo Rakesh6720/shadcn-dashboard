8c98fd6b3810ad3480fdeeed4e804900e58d91fa
Rakesh Baruah <rbaruah@crisisprevention.com>
2025-04-25 12:50:49 -0500

    fix: correct toggleSidebar function name in Navbar component

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -18,7 +18,7 @@ import { SidebarTrigger, useSidebar } from "./ui/sidebar";
 
 export default function Navbar() {
   const { setTheme } = useTheme();
-  const { toggleSideBar } = useSidebar();
+  const { toggleSidebar } = useSidebar();
   return (
     <nav className="p-4 flex items-center justify-between">
       <SidebarTrigger />
@@ -29,7 +29,7 @@ export default function Navbar() {
         <Link href="/">Dashboard</Link>
         <DropdownMenu>
           <DropdownMenuTrigger asChild>
-            <Button variant="outline" size="icon" onClick={toggleSideBar}>
+            <Button variant="outline" size="icon" onClick={toggleSidebar}>
               <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
               <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
               <span className="sr-only">Toggle theme</span>
